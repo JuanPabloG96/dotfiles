51b16fa08320261f884442a0c31680c1603fb1fb
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2025-02-16 04:20:57 -0700

    update: ls alias

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
   set fish_greeting
   oh-my-posh init fish --config ~/.config/ohMyPosh/custom.omp.json | source
 
-  alias ls='exa --group-directories-first --icons --color=always --git'
-  alias la='exa -a --group-directories-first --icons --color=always --git'
-  alias ll='exa -l --group-directories-first --icons --color=always --git'
+  alias ls='exa -l --group-directories-first --icons --color=always --git'
+  alias la='exa -l -a --group-directories-first --icons --color=always --git'
 end
