9aeac375726b11511ba683f0ec7f2aebfd177c86
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2025-05-08 02:28:37 -0600

    add: fish alias for logout, fastfetch command

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,11 @@ if status is-interactive
 
   alias ls='exa -l --group-directories-first --icons --color=always --git'
   alias la='exa -l -a --group-directories-first --icons --color=always --git'
+  alias logout='loginctl terminate-user $USER'
 end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+fastfetch
