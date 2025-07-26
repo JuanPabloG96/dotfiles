7259a994f2266a197827d8dd563f3d7a81757417
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2025-07-26 19:49:24 +0000

    fix: background transparency

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,7 @@ return {
   "tiagovla/tokyodark.nvim",
   opts = {
     -- custom options here
+    transparent_background = true,
   },
   config = function(_, opts)
     require("tokyodark").setup(opts) -- calling setup is optional
