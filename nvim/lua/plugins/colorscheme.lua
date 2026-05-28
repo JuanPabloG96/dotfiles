83a2429340584b90a1d8604b6ccbc61d4b4b7acd
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2026-05-28 04:24:29 -0600

    update: colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,5 +5,15 @@ return {
   opts = {
     style = "night",
     transparent = true,
+
+    styles = {
+      comments = { italic = true },
+      keywords = { italic = true },
+      functions = {},
+      variables = {},
+
+      sidebars = "transparent",
+      floats = "transparent",
+    },
   },
 }
